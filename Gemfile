e80629040f660f3581c41fd1ea4088fb1b9c0686
Kenneth <kltcalamay@gmail.com>
2011-03-23 16:00:03 +0800

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'mysql2'
+gem 'unicorn'
+gem "devise"
+gem "less"
+gem "redis"
+gem "slim"
+gem "slim-rails"
+
+group :development, :test do
+  gem 'cucumber-rails'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ms::Application.config.secret_token = '10b28e3035c735cf61d7dc443b0b1779faa7e89607e2d8bccc34a51348fc13ca0eb8288d657beccad6c91c94f126ec3eac4c3d7a3e97541cdf1f6646a123be32'
